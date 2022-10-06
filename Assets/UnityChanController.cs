34347ee473d5bcad5ac659dd975d5b14bdd29437
hundred-four <112915629+hundred-four@users.noreply.github.com>
2022-10-06 18:07:19 +0900

    各種プレハブ作成など

--- Assets/UnityChanController.cs
+++ Assets/UnityChanController.cs
@@ -8,6 +8,7 @@ public class UnityChanController : MonoBehaviour
     private Rigidbody myRigidbody;
     private float velocityZ = 16f;
     private float velocityX = 10f;
+    private float velocityY = 10f;
     private float movableRange = 3.4f;
        
     // Start is called before the first frame update
@@ -22,6 +23,7 @@ public class UnityChanController : MonoBehaviour
     void Update()
     {
         float inputVelocityX = 0;
+        float inputVelocityY = 0;
         this.myRigidbody.velocity = new Vector3(0, 0, this.velocityZ);
         if (Input.GetKey(KeyCode.LeftArrow) && -movableRange < this.transform.position.x)
         {
@@ -31,6 +33,21 @@ public class UnityChanController : MonoBehaviour
         {
             inputVelocityX = this.velocityX;
         }
-        this.myRigidbody.velocity = new Vector3(inputVelocityX, 0, velocityZ);
+
+        if (Input.GetKeyDown(KeyCode.Space) && this.transform.position.y <= 0.5f)
+        {
+            inputVelocityY = this.velocityY;
+            this.myAnimator.SetBool("Jump", true);
+        }
+        else
+        {
+            inputVelocityY = this.myRigidbody.velocity.y;
+        }
+
+        if (this.myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Jump"))
+        {
+            this.myAnimator.SetBool("Jump", false);
+        }
+        this.myRigidbody.velocity = new Vector3(inputVelocityX, inputVelocityY, velocityZ);
     }
 }
